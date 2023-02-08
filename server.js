3c3c6e6796c92bd788b79f6fa6e905a370c249c5
KJ O'BRIEN <kjokjo219@gmail.com>
2023-02-08 17:18:31 +0000

    Revert "Adds mongodb, express js support and enables inserting into the database from the sign up field. TODO: find out to combine/edit inserts to add additinal info that gets inserted on other pages"
    This reverts commit 012349ed75885a473c0b97554321ae940d4b9c33.

--- server.js
+++ server.js
@@ -1,33 +1,9 @@
 //nodemon server.js to run server
 import express from "express";
-import mongodb from "mongodb";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
 
 const server = express();
 
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/stride");
-mongoose.set('strictQuery', false);
-const schema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
-});
-const User = mongoose.model("User", schema);
 server.use(express.static("public"));
-server.use(bodyParser.urlencoded({ extended: true }));
-
-server.post("/user-signup", (req, res) =>{
-    var myData = new User(req.body);
-    myData.save();
-    res.redirect("/sethabits.html");
-    /*
-    .then(item => {
-        res.send("item saved to database");
-    });
-    */
-});
 
 server.listen(3000, ()=>{
     console.log("server up");
