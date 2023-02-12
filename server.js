a6944377b725273eaad8373f28d5968532e97232
KJ O'BRIEN <kjokjo219@gmail.com>
2023-02-12 14:55:07 +0000

    merges i think

--- server.js
+++ server.js
@@ -3,8 +3,43 @@ import express from "express";
 import mongodb from "mongodb";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-
+import cookieParser from "cookie-parser"
 const server = express();
+
+server.use(cookieParser());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(express.static("public"));
+
+
+server.get('/signup', (req, res) => {
+    res.cookie("SignUpCookie","KJ");
+    res.redirect("signup.html")
+});
+server.get('/about', (req, res) => {
+    res.redirect("about.html")
+});
+server.get('/dashboard', (req, res) => {
+    res.redirect("dashboard.html")
+});
+server.get('/habits', (req, res) => {
+    res.redirect("habits.html")
+});
+server.get('/index', (req, res) => {
+    res.redirect("index.html")
+});
+server.get('/login', (req, res) => {
+    res.redirect("login.html")
+});
+server.get('/profile', (req, res) => {
+    res.redirect("profile.html")
+});
+server.get('/progress', (req, res) => {
+    res.redirect("progress.html")
+});
+server.get('/sethabits', (req, res) => {
+    res.redirect("sethabits.html")
+});
+
 
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/stride");
@@ -20,10 +55,10 @@ const userName = new mongoose.Schema({
 const UserSignUp = mongoose.model("User", userCreatationSchema); //User collection, info to add
 const User = mongoose.model("User", userCreatationSchema);
 
-server.use(express.static("public"));
-server.use(bodyParser.urlencoded({ extended: true }));
+
 
 server.post("/user-signup", (req, res) =>{
+    console.log("bye bye")
     let myData = new UserSignUp(req.body);
     myData.save();
     res.redirect("/sethabits.html");
